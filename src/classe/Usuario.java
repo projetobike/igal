a6b571f15ff2c3466b459b7a4bdd4e181164f998
Técnico em Informática - 2019.1 <169121742019.1@senacrjedu.local>
2020-02-19 19:40:40 -0300

    aa

---
+++ src/classe/Usuario.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classe;
+
+/**
+ *
+ * @author 169121742019.1
+ */
+public class Usuario {
+    private String usuario;
+    private String email;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    public String getEmail(){
+        return usuario;
+    }
+    public void setEmail(String email){
+        this.usuario = usuario;
+    }
+    
+}
+ 
+
